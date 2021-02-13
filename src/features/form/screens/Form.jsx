e5db050d69e481c22f499a54b71836c9af11ce89
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-12 22:47:32 -0800

    feat: Create a basic CV Form

---
+++ src/features/form/screens/Form.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PrintButton from '../../../shared/Button/PrintButton';
+import FormFieldset from '../components/FormFieldset';
+import FormLayout from '../components/FormLayout';
+import { TextInput } from '../components/Inputs';
+
+class Form extends Component {
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <FormLayout>
+                <FormFieldset
+                    title="Personal Information"
+                    description="Share your contact information so that companies know how to reach you."
+                >
+                    <TextInput
+                        id="email"
+                        name="email-input"
+                        placeholder="e.g. yorktown5@gmail.com"
+                        title="Enter email info"
+                        value=""
+                    />
+                </FormFieldset>
+                <PrintButton />
+            </FormLayout>
+        );
+    }
+}
+
+export default Form;
