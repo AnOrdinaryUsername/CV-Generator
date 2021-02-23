f2ae40c916ea87628fa22a47ebdd4dce5e5a3668
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-22 19:13:32 -0800

    feat: Add onSubmit prop to form

--- src/features/form/components/FormLayout.jsx
+++ src/features/form/components/FormLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './FormLayout.css';
 
-const FormLayout = ({ children }) => {
+const FormLayout = ({ children, onSubmit }) => {
     return (
         <section className="form-container">
-            <form className="form form--round">{children}</form>
+            <form className="form form--round" onSubmit={onSubmit}>
+                {children}
+            </form>
         </section>
     );
 };
