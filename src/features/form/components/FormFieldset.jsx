783a4f5a54620a8a59d66bcc6676a23a8c21d57d
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-22 19:13:56 -0800

    feat: Adjust the name attr for every new inputs
    To differentiate between initial input names and newly created ones,
    add a number at the end of each name. The number depends on the
    length of this.state.info, which represents how many <NewInputs />
    were created.
    
    Ex. Given the initial inputs and 1 added new inputs. The new inputs
    would have a '1' appended at the end.
    
    	Initial input name ----> 'schoolName'
    	The 1 new input name --> 'schoolName1'

--- src/features/form/components/FormFieldset.jsx
+++ src/features/form/components/FormFieldset.jsx
@@ -18,11 +18,34 @@ class FormFieldset extends Component {
     addNewInfo(event) {
         event.preventDefault();
 
+        /*
+         * Deep clone an array so that newInputs doesn't refer to the same props.inputs from Form.jsx.
+         * The inputs data only contains strings so it's okay in this case.
+         * https://stackoverflow.com/a/122704
+         * https://stackoverflow.com/a/23481096
+         */
+        const newInputs = JSON.parse(JSON.stringify(this.props.inputs));
+
+        // Assign an index to each input's name attribute for differentiation.
+        // Ex. name = 'schoolName0' for a newly added info input
+        newInputs.forEach((element) => {
+            const index = this.state.info.length;
+
+            if (element.row) {
+                element.row.forEach((element) => {
+                    return (element.name = `${element.name}${index.toString()}`);
+                });
+            }
+
+            return (element.name = `${element.name}${index.toString()}`);
+        });
+
         const inputs = (
             <NewInputs
-                inputs={this.props.inputs}
+                inputs={newInputs}
                 enableAnimation={true}
                 includeDelete={true}
+                onChange={this.props.onChange}
                 key={uniqid()}
             />
         );
@@ -38,9 +61,13 @@ class FormFieldset extends Component {
         // reason to add more personal info.
         const isPersonalFieldset = title === 'Personal Information';
 
-        // Initial inputs.
-        const inputs = (
-            <NewInputs inputs={this.props.inputs} enableAnimation={false} includeDelete={false} />
+        const initialInputs = (
+            <NewInputs
+                inputs={this.props.inputs}
+                enableAnimation={false}
+                includeDelete={false}
+                onChange={this.props.onChange}
+            />
         );
         const { info } = this.state;
 
@@ -51,7 +78,7 @@ class FormFieldset extends Component {
                     <p>{description}</p>
                 </legend>
                 <div className="input-container">
-                    {inputs}
+                    {initialInputs}
                     {info.length !== 0 && info.map((element) => element)}
                     {!isPersonalFieldset && <NewInfoButton onClick={this.addNewInfo} />}
                 </div>
