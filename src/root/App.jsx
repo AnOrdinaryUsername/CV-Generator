62eb990064c4209b772c5e89202d0acc0acbfe47
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-22 18:58:33 -0800

    feat: Lift the Input state up to App
    Now that App can access the values of the inputs, we can use the state
    to create a future PDF component.

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,15 +1,48 @@
+import React, { Component } from 'react';
 import Form from '../features/form/screens/Form';
 import Header from '../features/navbar/screens/Header';
 
-function App() {
-    return (
-        <>
-            <Header />
-            <main>
-                <Form />
-            </main>
-        </>
-    );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            isSubmitted: false,
+        };
+
+        this.submitForm = this.submitForm.bind(this);
+        this.handleFieldChange = this.handleFieldChange.bind(this);
+    }
+
+    submitForm() {
+        this.setState({
+            isSubmitted: !this.state.isSubmitted,
+        });
+    }
+
+    // Callback used in 'handleChange()' in NewInputs.jsx + CustomEditor.jsx
+    handleFieldChange(name, value) {
+        this.setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    }
+
+    render() {
+        const { isSubmitted } = this.state;
+
+        return (
+            <>
+                <Header />
+                <main>
+                    {!isSubmitted && (
+                        <Form onSubmit={this.submitForm} onChange={this.handleFieldChange} />
+                    )}
+                    {isSubmitted && <p>test</p>}
+                </main>
+            </>
+        );
+    }
 }
 
 export default App;
