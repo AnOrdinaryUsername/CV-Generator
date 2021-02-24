e5c0d42fd95df723df0f68a9ccd870e19c80d108
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-23 23:18:42 -0800

    feat: Add click handler to DownloadButton
    html2canvas + jspdf does seem to work but doesn't really scale well
    on different viewports. Maybe replace with react-pdf if it doesn't
    work out.

--- src/shared/Buttons/Buttons.jsx
+++ src/shared/Buttons/Buttons.jsx
@@ -1,5 +1,7 @@
 import { faDownload, faPrint } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 import React from 'react';
 import './Buttons.css';
 
@@ -14,9 +16,25 @@ const PrintButton = () => {
     );
 };
 
-const DownloadButton = ({ onClick }) => {
+const DownloadButton = ({ firstName, lastName }) => {
+    const downloadPdf = () => {
+        const display = document.getElementById('pdf-container');
+
+        html2canvas(display).then((canvas) => {
+            const image = canvas.toDataURL('image/png');
+            const pdf = new jsPDF();
+
+            const imageProps = pdf.getImageProperties(image);
+            const pdfWidth = pdf.internal.pageSize.getWidth();
+            const pdfHeight = (imageProps.height * pdfWidth) / imageProps.width;
+
+            pdf.addImage(image, 'PNG', 0, 0, pdfWidth, pdfHeight);
+            pdf.save(`${firstName}${lastName}CV.pdf`);
+        });
+    };
+
     return (
-        <button className="button button--md download" onClick={onClick}>
+        <button className="button button--md download" onClick={downloadPdf}>
             <FontAwesomeIcon icon={faDownload} />
             Download as PDF
         </button>
