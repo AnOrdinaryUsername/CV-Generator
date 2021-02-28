ce19c0f616dee1618a4d44a36ae86b295fa5f66f
AnOrdinaryUsername <kylemasa2@gmail.com>
2021-02-27 20:07:55 -0800

    refactor: Use controlled components in App
    Previously, input values weren't stored in App state and was passed
    as data in Form.jsx. At the time it was fine; the data was still being
    passed to handleInputChange and processed to make a pdf. Only problem
    was the addition of a new feature: persisting state through localStorage.
    
    This soon proved to problematic as Form's data, this.formSection,
    never passed a value prop to <Input />, which in turn alway left
    the value attribute as undefined. Upon addition of a value string to
    this.formSection, the input empties on every rerender, causing it to only
    take a single character.
    
    To solve this and have localStorage capabilities, include the value in
    state and pass it to children as props.

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,19 +1,117 @@
-import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import { PDFViewer } from '@react-pdf/renderer';
 import React, { Component } from 'react';
 import Form from '../features/form/screens/Form';
 import Header from '../features/navbar/screens/Header';
 import PDF from '../features/pdf/screens/PDF';
 
+// object => object literal
+// prop => string (the property inside the object literal)
+// return boolean
+const objectHasProperty = (object, prop) => ({}.propertyIsEnumerable.call(object, prop));
+
 class App extends Component {
     constructor() {
         super();
 
+        let storage = null;
+        if (localStorage) {
+            storage = { ...localStorage };
+            // No need to store color theme in App state.
+            delete storage['theme'];
+        }
+
         this.state = {
             isSubmitted: false,
+            personal: [
+                {
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    phoneNumber: '',
+                    residence: '',
+                },
+            ],
+            education: [
+                {
+                    schoolName: '',
+                    fieldOfStudy: '',
+                    location: '',
+                    date: '',
+                    editor:
+                        '<ul><li><strong>Current GPA</strong>: 4.0</li><li><strong>Projects</strong>: Facebook clone, Battleship</li></ul>',
+                },
+            ],
+            work: [
+                {
+                    companyName: '',
+                    jobTitle: '',
+                    location: '',
+                    date: '',
+                    editor:
+                        '<ul><li>Coordinated movement of air wings at the Battle of Midway.</li></ul>',
+                },
+            ],
         };
 
         this.submitForm = this.submitForm.bind(this);
         this.handleFieldChange = this.handleFieldChange.bind(this);
+        this.updateInputCount = this.updateInputCount.bind(this);
+    }
+
+    updateInputCount(sectionName, index) {
+        switch (sectionName) {
+            case 'education': {
+                const schoolName = `schoolName${index}`;
+                const fieldOfStudy = `fieldOfStudy${index}`;
+                const location = `location${index}`;
+                const date = `date${index}`;
+                const editor = `editor${index}`;
+
+                this.setState((prevState) => ({
+                    ...prevState,
+                    education: [
+                        ...prevState.education,
+                        {
+                            [schoolName]: '',
+                            [fieldOfStudy]: '',
+                            [location]: '',
+                            [date]: '',
+                            [editor]:
+                                '<ul><li><strong>Current GPA</strong>: 4.0</li><li><strong>Projects</strong>: Facebook clone, Battleship</li></ul>',
+                        },
+                    ],
+                }));
+                break;
+            }
+            case 'work': {
+                const companyName = `companyName${index}`;
+                const jobTitle = `jobTitle${index}`;
+                const location = `location${index}`;
+                const date = `date${index}`;
+                const editor = `editor${index}`;
+
+                this.setState((prevState) => ({
+                    ...prevState,
+                    work: [
+                        ...prevState.work,
+                        {
+                            [companyName]: '',
+                            [jobTitle]: '',
+                            [location]: '',
+                            [date]: '',
+                            [editor]:
+                                '<ul><li>Coordinated movement of air wings at the Battle of Midway.</li></ul>',
+                        },
+                    ],
+                }));
+                break;
+            }
+            default:
+                throw new Error(
+                    `Unexpected section name '${sectionName}' was passed.` +
+                        "Only valid names are 'work' and 'education'."
+                );
+        }
     }
 
     submitForm() {
@@ -23,11 +121,58 @@ class App extends Component {
     }
 
     // Callback used in 'handleChange()' in NewInputs.jsx + CustomEditor.jsx
-    handleFieldChange(name, value) {
-        this.setState((prevState) => ({
-            ...prevState,
-            [name]: value,
-        }));
+    handleFieldChange(sectionName, name, value) {
+        switch (sectionName) {
+            case 'personal':
+                this.setState(
+                    (prevState) => ({
+                        ...prevState,
+                        personal: [
+                            {
+                                ...prevState.personal[0],
+                                [name]: value,
+                            },
+                        ],
+                    }),
+                    () => {
+                        localStorage.setItem(name, value);
+                    }
+                );
+                break;
+            case 'education':
+                this.setState(
+                    (prevState) => ({
+                        ...prevState,
+                        education: prevState.education.map((state) => {
+                            const matchingName = objectHasProperty(state, name);
+                            return matchingName ? { ...state, [name]: value } : state;
+                        }),
+                    }),
+                    () => {
+                        localStorage.setItem(name, value);
+                    }
+                );
+                break;
+            case 'work':
+                this.setState(
+                    (prevState) => ({
+                        ...prevState,
+                        work: prevState.work.map((state) => {
+                            const matchingName = objectHasProperty(state, name);
+                            return matchingName ? { ...state, [name]: value } : state;
+                        }),
+                    }),
+                    () => {
+                        localStorage.setItem(name, value);
+                    }
+                );
+                break;
+            default:
+                throw new Error(
+                    `Unexpected section name '${sectionName}' was passed.` +
+                        "Only valid names are 'personal', 'work' and 'education'."
+                );
+        }
     }
 
     render() {
@@ -44,18 +189,20 @@ class App extends Component {
                 <Header />
                 <main>
                     {!isSubmitted && (
-                        <Form onSubmit={this.submitForm} onChange={this.handleFieldChange} />
+                        <Form
+                            onSubmit={this.submitForm}
+                            onChange={this.handleFieldChange}
+                            updateInputCount={this.updateInputCount}
+                            storedInputs={this.state}
+                        />
                     )}
                     {isSubmitted && (
                         <>
-                            <PDFViewer width={'100%'} height={'100%'}>
+                            <PDFViewer width={'80%'} height={'100%'}>
                                 {UserPDF}
                             </PDFViewer>
-                            <PDFDownloadLink document={UserPDF} fileName={`${userName}.pdf`}>
-                                {({ blob, url, loading, error }) =>
-                                    loading ? 'Loading document...' : 'Download now!'
-                                }
-                            </PDFDownloadLink>
+
+                            <button onClick={this.submitForm}>{'< Go back'}</button>
                         </>
                     )}
                 </main>
